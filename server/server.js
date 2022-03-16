813c88f1a19e02a5dfa1967c1b5f4273ff885441
amielau <amielperkins@gmail.com>
2022-03-16 12:17:02 -0600

    deployment update

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ app.use(express.json());
 
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../index.html"))
+    res.sendFile(path.join(__dirname, "../public/index.html", "../index.html"))
+});
+
+app.get("/styles", function(req,res) {
+    res.sendFile(path.join(__dirname, "..public/index.css"));
 });
 
 const port = process.env.PORT || 4005;
