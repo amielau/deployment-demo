d7b39cea84f27581f9622b5bd61dd11a4f3a9b36
amielau <amielperkins@gmail.com>
2022-03-16 12:30:32 -0600

    new update

--- server/server.js
+++ server/server.js
@@ -8,13 +8,19 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html", "../index.html"))
-});
+//middleware
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/styles", function(req,res) {
-    res.sendFile(path.join(__dirname, "..public/index.css"));
-});
+// app.get("/", function(req, res) {
+    //     res.sendFile(path.join(__dirname, "../public "))
+    // });
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"))
+// });
+
+// app.get("/styles", function(req,res) {
+//     res.sendFile(path.join(__dirname, "..public/index.css"));
+// });
 
 const port = process.env.PORT || 4005;
 
